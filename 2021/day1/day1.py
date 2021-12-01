3936e385d2319fb88b2a5c23f35fd1aaa5dff0c8
ollikaven <olli.kaven@gmail.com>
2021-12-01 15:15:52 +0200

    Start index from 1

--- 2021/day1/day1.py
+++ 2021/day1/day1.py
@@ -1,17 +1,14 @@
 import pandas as pd
 
-with open("input.txt", "r") as file:
+with open("2021/day1/input.txt", "r") as file:
     _input = [int(x.strip()) for x in file.readlines()]
 
 #Part 1
-i = 0
+i = 1
 increases = 0
 while i < len(_input):
-    if i == 0:
-        pass
-    else:
-        if _input[i] > _input[i-1]:
-            increases = increases + 1
+    if _input[i] > _input[i-1]:
+        increases = increases + 1
     i = i+1
 
 print(f"day1: {increases}")
