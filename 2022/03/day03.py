57227fa5b0f6586ed318e7f31175e7e2b8e250c2
ollikaven <olli.kaven@gmail.com>
2022-12-03 14:01:56 +0200

    refactor

--- 2022/03/day03.py
+++ 2022/03/day03.py
@@ -1,11 +1,8 @@
 lines = list(map(str.strip, open("input.txt").readlines()))
 
-common = [next(iter(set([char for char in x[:len(x)//2] if char in x[len(x)//2:]]))) for x in lines]
+def result(common):
+    return sum([ord(x)-96 if x.islower() else ord(x)-38 for x in common])
 
-# Part 1
-print(sum([ord(x)-96 if x.islower() else ord(x)-38 for x in common]))
+print(result([next(iter(set([char for char in x[:len(x)//2] if char in x[len(x)//2:]]))) for x in lines]))
+print(result([next(iter(set.intersection(*map(set,[lines[i],lines[i+1],lines[i+2]])))) for i in range(0, len(lines), 3)]))
 
-# Part 2
-common = [next(iter(set.intersection(*map(set,[lines[i],lines[i+1],lines[i+2]])))) for i in range(0, len(lines), 3)]
-
-print(sum([ord(x)-96 if x.islower() else ord(x)-38 for x in common]))
